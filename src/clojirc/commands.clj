264f69da5bad693c330bad8f6ba5aff328b04304
Mikael Kragbæk <mikael@kragbaek.com>
2014-04-27 15:00:53 +0200

    Added ENCAP to ISON
    Added a function for each of the following commands:
    ENCAP
    ERROR
    HELP
    INFO
    INVITE
    ISON

--- src/clojirc/commands.clj
+++ src/clojirc/commands.clj
@@ -135,6 +135,114 @@
     network
     "DIE"))
 
+(defn encap!
+  "Parameters: <source> <destination> <subcommand> [ <parameters> ]
+  
+  This command is for use by servers to encapsulate commands so that 
+  they will propagate across hub servers not yet updated to support 
+  them, and indicates the subcommand and its parameters should be 
+  passed unaltered to the destination, where it will be unencapsulated 
+  and parsed. This facilitates implementation of new features without a 
+  need to restart all servers before they are usable across the network."
+  [network source destination subcommand & parameters]
+  (network-send
+    network
+    (match [parameters]
+           [nil] (format ":%s ENCAP %s %s" source destination subcommand)
+           [_] (format ":%s ENCAP %s %s %s" source destination subcommand (clojure.string/join " " parameters)))))
+
+(defn error!
+  "Parameters: <error message>
+
+   The ERROR command is for use by servers when reporting a serious or
+   fatal error to its peers.  It may also be sent from one server to
+   another but MUST NOT be accepted from any normal unknown clients.
+
+   Only an ERROR message SHOULD be used for reporting errors which occur
+   with a server-to-server link.  An ERROR message is sent to the server
+   at the other end (which reports it to appropriate local users and
+   logs) and to appropriate local users and logs.  It is not to be
+   passed onto any other servers by a server if it is received from a
+   server.
+
+   The ERROR message is also used before terminating a client
+   connection.
+
+   When a server sends a received ERROR message to its operators, the
+   message SHOULD be encapsulated inside a NOTICE message, indicating
+   that the client was not responsible for the error."
+   [network error-message]
+   (network-send
+     network
+     (format "ERROR %s" error-message)))
+
+(defn help!
+  "Parameters: None
+  Requests the server help file.
+
+  This command is not formally defined in an RFC, but is in use by most 
+  major IRC daemons."
+  [network]
+  (network-send
+    network
+    "HELP"))
+
+(defn info!
+  "Parameters: [ <target> ]
+
+   The INFO command is REQUIRED to return information describing the
+   server: its version, when it was compiled, the patchlevel, when it
+   was started, and any other miscellaneous information which may be
+   considered to be relevant.
+
+   Wildcards are allowed in the <target> parameter."
+   [network & [target]]
+   (network-send
+     network
+     (match [target]
+            [nil] "INFO"
+            [_] (format "INFO %s" target))))
+
+(defn invite!
+  "Parameters: <nickname> <channel>
+
+   The INVITE command is used to invite a user to a channel.  The
+   parameter <nickname> is the nickname of the person to be invited to
+   the target channel <channel>.  There is no requirement that the
+   channel the target user is being invited to must exist or be a valid
+   channel.  However, if the channel exists, only members of the channel
+   are allowed to invite other users.  When the channel has invite-only
+   flag set, only channel operators may issue INVITE command."
+   [network nickname channel]
+   (network-send
+     network
+     (format "INVITE %s %s" nickname channel)))
+
+(defn ison!
+  "Parameters: <nickname> *( SPACE <nickname> )
+
+   The ISON command was implemented to provide a quick and efficient
+   means to get a response about whether a given nickname was currently
+   on IRC. ISON only takes one (1) type of parameter: a space-separated
+   list of nicks.  For each nickname in the list that is present, the
+   server adds that to its reply string.  Thus the reply string may
+   return empty (none of the given nicks are present), an exact copy of
+   the parameter string (all of them present) or any other subset of the
+   set of nicks given in the parameter.  The only limit on the number of
+   nicks that may be checked is that the combined length MUST NOT be too
+   large as to cause the server to chop it off so it fits in 512
+   characters.
+
+   ISON is only processed by the server local to the client sending the
+   command and thus not passed onto other servers for further
+   processing."
+   [network nickname & more-nicknames]
+   (network-send
+     network
+     (match [more-nicknames]
+            [nil] (format "ISON %s" nickname)
+            [_] (format "ISON %s %s" nickname (clojure.string/join " " more-nicknames)))))
+
 (defn join!
   [network chan & [pass]]
   (network-send
